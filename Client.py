b755c8a480d2e47e6a5f87beebf5114c02538598
Bjørn Hoxmark <bhoxmark@gmail.com>
2017-04-30 10:31:16 -0700

    First server and client

---
+++ Client.py
@@ -0,0 +1,56 @@
+import struct
+import socket
+# class MySocket:
+#     """demonstration class only
+#       - coded for clarity, not efficiency
+#     """
+
+#     def __init__(self, sock=None):
+#         if sock is None:
+#             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#         else:
+#             self.sock = sock
+
+#     def connect(self, host, port):
+#         self.sock.connect((host, port))
+
+#     def mysend(self, msg):
+#         totalsent = 0
+#         while totalsent < MSGLEN:
+#             sent = self.sock.send(msg[totalsent:])
+#             if sent == 0:
+#                 raise RuntimeError("socket connection broken")
+#             totalsent = totalsent + sent
+
+#     def myreceive(self):
+#         chunks = []
+#         bytes_recd = 0
+#         while bytes_recd < MSGLEN:
+#             chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
+#             if chunk == b'':
+#                 raise RuntimeError("socket connection broken")
+#             chunks.append(chunk)
+#             bytes_recd = bytes_recd + len(chunk)
+#         return b''.join(chunks)
+
+# print("Starting Client")
+# mySock = MySocket()
+
+
+# mySock.connect(socket.gethostname(), 8090)
+# mySock.mysend("Hello From the other side")
+
+
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 8090                # Reserve a port for your service.
+s.connect((host, port))
+
+message = "Hello From the other side\n"
+size = len(message)
+s.send(struct.pack("!H", size))
+s.send(message)
+s.close 
+
+
