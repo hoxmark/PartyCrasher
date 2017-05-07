c8ff9926a900a4eac2e35f2d60b4f82554ac8469
Bjørn Hoxmark <bhoxmark@gmail.com>
2017-05-06 17:49:57 -0700

    correct static IP

--- random-flip.c
+++ random-flip.c
@@ -180,7 +180,7 @@ int build_socket() {
     }
     // puts("Socket created");
 
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_addr.s_addr = inet_addr("104.198.30.238");
     server.sin_family = AF_INET;
     server.sin_port = htons(5000);
 
