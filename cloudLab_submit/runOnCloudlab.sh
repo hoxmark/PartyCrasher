a926e6be8b5288978fdfeb71acceb37a741522ed
Bjørn Hoxmark <bhoxmark@gmail.com>
2017-05-15 21:47:49 -0700

    cleanup added new scripts for cloudlab

---
+++ cloudLab_submit/runOnCloudlab.sh
@@ -0,0 +1,32 @@
+rm clique-count.c
+rm nextwork.c
+rm nextwork.o
+
+wget https://raw.githubusercontent.com/hoxmark/PartyCrasher/master/clique-count.c?token=AFLKRyMWWp4cMqRWIFqRZzj3L9idLv8Yks5ZInf0wA%3D%3D
+mv clique-count.c?token=AFLKRyMWWp4cMqRWIFqRZzj3L9idLv8Yks5ZInf0wA%3D%3D clique-count.c
+
+wget https://raw.githubusercontent.com/hoxmark/PartyCrasher/master/nextwork.c?token=AFLKR9W-k5uahnJmC-VzsdTJXCCXhma4ks5ZI6kswA%3D%3D
+mv nextwork.c?token=AFLKR9W-k5uahnJmC-VzsdTJXCCXhma4ks5ZI6kswA%3D%3D nextwork.c
+
+gcc nextwork.c -o nextwork.o
+
+./nextwork.o 104.198.30.238 5004 3 1 &
+./nextwork.o 104.198.30.238 5004 3 1 &
+./nextwork.o 104.198.30.238 5004 3 1 &
+./nextwork.o 104.198.30.238 5004 3 1 &
+./nextwork.o 104.198.30.238 5004 3 1 &
+./nextwork.o 104.198.30.238 5004 3 1 &
+./nextwork.o 104.198.30.238 5004 3 1 &
+./nextwork.o 104.198.30.238 5004 3 1 &
+./nextwork.o 104.198.30.238 5004 3 1 &
+./nextwork.o 104.198.30.238 5004 3 1 &
+./nextwork.o 104.198.30.238 5004 3 1 &
+./nextwork.o 104.198.30.238 5004 3 1 &
+./nextwork.o 104.198.30.238 5004 3 1 &
+./nextwork.o 104.198.30.238 5004 3 1 &
+./nextwork.o 104.198.30.238 5004 3 1 &
+./nextwork.o 104.198.30.238 5004 3 1 &
+./nextwork.o 104.198.30.238 5004 3 1 &
+./nextwork.o 104.198.30.238 5004 3 1 &
+./nextwork.o 104.198.30.238 5004 3 1 &
+./nextwork.o 104.198.30.238 5004 3 1 &
