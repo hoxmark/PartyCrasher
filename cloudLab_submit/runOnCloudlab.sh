3f3dbcd03d4f44952a5259b17f549f13d51d891c
Bjørn Hoxmark <bhoxmark@gmail.com>
2017-05-16 13:16:36 -0700

    script change

--- cloudLab_submit/runOnCloudlab.sh
+++ cloudLab_submit/runOnCloudlab.sh
@@ -2,31 +2,56 @@ rm clique-count.c
 rm nextwork.c
 rm nextwork.o
 
-wget https://raw.githubusercontent.com/hoxmark/PartyCrasher/master/clique-count.c?token=AFLKRyMWWp4cMqRWIFqRZzj3L9idLv8Yks5ZInf0wA%3D%3D
-mv clique-count.c?token=AFLKRyMWWp4cMqRWIFqRZzj3L9idLv8Yks5ZInf0wA%3D%3D clique-count.c
+wget https://raw.githubusercontent.com/hoxmark/PartyCrasher/master/clique-count.c?token=AFLKR06Wku2LIRLCYc00rOM6GFNtdj9eks5ZI8GxwA%3D%3D
+mv clique-count.c?token=AFLKR06Wku2LIRLCYc00rOM6GFNtdj9eks5ZI8GxwA%3D%3D clique-count.c
+
+wget https://raw.githubusercontent.com/hoxmark/PartyCrasher/master/clique-count-extend.c?token=AFLKR4ap7cEJNzGLYiu1CU-gyqXcHnbpks5ZI8J5wA%3D%3D
+mv clique-count-extend.c?token=AFLKR4ap7cEJNzGLYiu1CU-gyqXcHnbpks5ZI8J5wA%3D%3D clique-count-extend.c
 
 wget https://raw.githubusercontent.com/hoxmark/PartyCrasher/master/nextwork.c?token=AFLKR9W-k5uahnJmC-VzsdTJXCCXhma4ks5ZI6kswA%3D%3D
 mv nextwork.c?token=AFLKR9W-k5uahnJmC-VzsdTJXCCXhma4ks5ZI6kswA%3D%3D nextwork.c
 
 gcc nextwork.c -o nextwork.o
 
-./nextwork.o 104.198.30.238 5004 3 1 &
-./nextwork.o 104.198.30.238 5004 3 1 &
-./nextwork.o 104.198.30.238 5004 3 1 &
-./nextwork.o 104.198.30.238 5004 3 1 &
-./nextwork.o 104.198.30.238 5004 3 1 &
-./nextwork.o 104.198.30.238 5004 3 1 &
-./nextwork.o 104.198.30.238 5004 3 1 &
-./nextwork.o 104.198.30.238 5004 3 1 &
-./nextwork.o 104.198.30.238 5004 3 1 &
-./nextwork.o 104.198.30.238 5004 3 1 &
-./nextwork.o 104.198.30.238 5004 3 1 &
-./nextwork.o 104.198.30.238 5004 3 1 &
-./nextwork.o 104.198.30.238 5004 3 1 &
-./nextwork.o 104.198.30.238 5004 3 1 &
-./nextwork.o 104.198.30.238 5004 3 1 &
-./nextwork.o 104.198.30.238 5004 3 1 &
-./nextwork.o 104.198.30.238 5004 3 1 &
-./nextwork.o 104.198.30.238 5004 3 1 &
-./nextwork.o 104.198.30.238 5004 3 1 &
-./nextwork.o 104.198.30.238 5004 3 1 &
+./nextwork.o 104.198.30.238 5004 3 1 >>log.txt & 
+disown
+./nextwork.o 104.198.30.238 5004 3 1 >>log.txt &
+disown
+./nextwork.o 104.198.30.238 5004 3 1 >>log.txt &
+disown
+./nextwork.o 104.198.30.238 5004 3 1 >>log.txt &
+disown
+./nextwork.o 104.198.30.238 5004 3 1 >>log.txt &
+disown
+./nextwork.o 104.198.30.238 5004 3 1 >>log.txt &
+disown
+./nextwork.o 104.198.30.238 5004 3 1 >>log.txt &
+disown
+./nextwork.o 104.198.30.238 5004 3 1 >>log.txt &
+disown
+./nextwork.o 104.198.30.238 5004 3 1 >>log.txt &
+disown
+./nextwork.o 104.198.30.238 5004 3 1 >>log.txt &
+disown
+./nextwork.o 104.198.30.238 5004 3 1 >>log.txt &
+disown
+./nextwork.o 104.198.30.238 5004 3 1 >>log.txt &
+disown
+./nextwork.o 104.198.30.238 5004 3 1 >>log.txt &
+disown
+./nextwork.o 104.198.30.238 5004 3 1 >>log.txt &
+disown
+./nextwork.o 104.198.30.238 5004 3 1 >>log.txt &
+disown
+./nextwork.o 104.198.30.238 5004 3 1 >>log.txt &
+disown
+./nextwork.o 104.198.30.238 5004 3 1 >>log.txt &
+disown
+./nextwork.o 104.198.30.238 5004 3 1 >>log.txt &
+disown
+./nextwork.o 104.198.30.238 5004 3 1 >>log.txt &
+disown
+./nextwork.o 104.198.30.238 5004 3 1 >>log.txt &
+disown
+
+exit
