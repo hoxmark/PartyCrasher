97e7e55caea4f1040c3039ad4eab6697130dbdd1
root <root@cordinator-1.c.partycra-166217.internal>
2017-05-22 20:23:39 +0000

    Annealing threshold

--- Cordinator/src/Config.java
+++ Cordinator/src/Config.java
@@ -5,7 +5,7 @@ class Config {
     public static final String BESTLICUQE_ALGORITHM_NAME = "BestClique";
 
     public static final int CLIENT_UPDATE_INTERVAL_SECONDS = 100;
-    public static final int ANNEALING_THRESHOLD = 1000000;
+    public static final int ANNEALING_THRESHOLD = 2500000;
     public static final int ANNEALING_TIMEOUT_MS = CLIENT_UPDATE_INTERVAL_SECONDS * 1000;
     public static final String R_CONTINE = "CONTINUE";
     public static final String R_RETRY = "RETRY";
